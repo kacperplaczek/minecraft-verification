20a025d829decd9c823f93b22a9c3ea5c269cb47
Kacper Płaczek <xk.placzek@gmail.com>
2026-04-19 21:13:02 +0200

    update: contact_email in privacy-policy

--- src/features/privacy-policy/model.ts
+++ src/features/privacy-policy/model.ts
@@ -1,3 +1,3 @@
 export const SERVER_NAME = "rose.owo - Serwer Discord dla Graczy"
-export const CONTACT_EMAIL = "xk.placzek@gmail.com"
+export const CONTACT_EMAIL = "rose.owo@gmail.com"
 export const LAST_UPDATED = "19 kwietnia 2025"
